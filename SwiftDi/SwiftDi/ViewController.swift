95674f7926db5dd6100eecef13fbbb861b399f69
Yar177 <yarsher.dev@gmail.com>
2021-08-28 08:49:35 -0400

    init

---
+++ SwiftDi/SwiftDi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDi
+//
+//  Created by Hoshiar Sher on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
