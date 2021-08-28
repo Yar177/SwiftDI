d772de3f63e738a4586822aaa0848939a219a0d8
Yar177 <yarsher.dev@gmail.com>
2021-08-28 09:10:52 -0400

    btn

--- SwiftDi/SwiftDi/ViewController.swift
+++ SwiftDi/SwiftDi/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //"https://iosacademy.io/api/v1/courses/index.php"
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.setTitle("Tap Here", for: .normal)
+        button.center = view.center
+        button.setTitleColor(.white, for: .normalr)
     }
-
 
 }
 
